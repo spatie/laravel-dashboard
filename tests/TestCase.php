0d6fe17592d53a40d25de1294dbff2cc3f2da398
freek <freek@spatie.be>
2020-09-09 09:33:56 +0200

    --amend

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -32,23 +32,4 @@ abstract class TestCase extends Orchestra
         include_once __DIR__.'/../database/migrations/create_dashboard_tiles_table.php.stub';
         (new CreateDashboardTilesTable())->up();
     }
-
-    /*
-    protected function renderBladeString(string $bladeContent): string
-    {
-        $temporaryDirectory = sys_get_temp_dir();
-
-        if (! in_array($temporaryDirectory, View::getFinder()->getPaths())) {
-            View::addLocation(sys_get_temp_dir());
-        }
-
-        $tempFilePath = tempnam($temporaryDirectory, 'tests') . '.blade.php';
-
-        file_put_contents($tempFilePath, $bladeContent);
-
-        $bladeViewName = Str::before(pathinfo($tempFilePath, PATHINFO_BASENAME), '.blade.php');
-
-        return view($bladeViewName)->render();
-    }
-    */
 }
