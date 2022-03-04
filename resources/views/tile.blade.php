326c416edb5cf6dd6f8b9e2208c8382591960fed
Ayman Atmeh <ayman.atmeh@gmail.com>
2022-03-04 15:26:33 +0300

    Update tile.blade.php
    updated Tile.blade.php with merge attribute ... now you can add classes to tiles from blade view

--- resources/views/tile.blade.php
+++ resources/views/tile.blade.php
@@ -1,6 +1,8 @@
 <div
     style="grid-area: {{ $gridArea }};{{ $show ? '' : 'display:none' }}"
-    class="overflow-hidden rounded relative bg-tile"
+     {{ $attributes->merge([
+        'class'=>'overflow-hidden rounded relative bg-tile"'
+        ])}}
     {{ $refreshIntervalInSeconds ? "wire:poll.{$refreshIntervalInSeconds}s" : ''  }}
 >
     <div
