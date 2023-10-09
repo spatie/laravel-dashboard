69a6304d5316b6b608f11730fb0f3f1063447df3
Rias <hey@rias.be>
2023-10-09 10:07:35 +0200

    Add session expiration logic

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -63,6 +63,18 @@
             })
         },
     });
+
+    document.addEventListener('livewire:init', () => {
+        Livewire.hook('request', ({ fail }) => {
+            fail(({ status, preventDefault }) => {
+                if (status === 419) {
+                    preventDefault();
+
+                    window.location.reload();
+                }
+            })
+        });
+    });
 </script>
 
 
